cc9341aded7b687fd5f05448000b68701f907686
gspinoza <Espinoza.Gavriell@gmail.com>
2022-07-25 17:24:07 -0400

    added Variables.java

---
+++ src/main/java/gspinoza/Variables.java
@@ -0,0 +1,22 @@
+package gspinoza;
+
+// Variables - Store values and can't have more than one value. 
+public class Variables {
+
+    public static void main(String[] args) {
+        // declaring and initializing variables
+        String bird = "Chickadee";
+        String habitat = "North Amercia";
+        int numberOfSpecies = 7; 
+        double averageSize = 3.3; // inches
+        char initialLetter = 'C';
+        
+        // display values
+        System.out.println(bird);
+        System.out.println(habitat);
+        System.out.println(numberOfSpecies);
+        System.out.println(averageSize);
+        System.out.println(initialLetter);
+    }
+
+}
